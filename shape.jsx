7a31dc4589c05d125c9de2dca828b70a6e4eb3bc
GrayYoung <windpet@live.cn>
2015-12-10 10:35:55 +0800

    Draw circle

--- shape.jsx
+++ shape.jsx
@@ -2,21 +2,21 @@
 	vector : {
 		create : function() {
 			var doc = app.activeDocument;
-			var lineSubPathArray = new SubPathInfo();
-			var lineArray = [];
+			var subPath = new SubPathInfo();
+			var points = [];
 
 			for (var i = 0; i < arguments.length; i++) {
-				lineArray[i] = new PathPointInfo;
-				lineArray[i].kind = PointKind.SMOOTHPOINT;
-				lineArray[i].anchor = arguments[i];
-				lineArray[i].leftDirection = [ arguments[i][0] + 50, arguments[i][0] + 50 ];
-				lineArray[i].rightDirection = [ arguments[i][0] + 50, arguments[i][0] + 50 ];
+				points[i] = new PathPointInfo;
+				points[i].kind = PointKind.SMOOTHPOINT;
+				points[i].anchor = arguments[i];
+				points[i].leftDirection = [ arguments[i][0] + 50, arguments[i][0] + 50 ];
+				points[i].rightDirection = [ arguments[i][0] + 50, arguments[i][0] + 50 ];
 			}
 
-			lineSubPathArray.closed = true;
-			lineSubPathArray.operation = ShapeOperation.SHAPEADD;
-			lineSubPathArray.entireSubPath = lineArray;
-			var myPathItem = doc.pathItems.add("myPath", [lineSubPathArray]);
+			subPath.closed = true;
+			subPath.operation = ShapeOperation.SHAPEADD;
+			subPath.entireSubPath = points;
+			var myPathItem = doc.pathItems.add("myPath", [subPath]);
 
 			var desc88 = new ActionDescriptor();
 			var desc89 = new ActionDescriptor();
@@ -38,69 +38,69 @@
 			executeAction(charIDToTypeID("Mk  "), desc88, DialogModes.NO);
 			myPathItem.remove();
 		},
-		createCycle : function(size, cPosition) {
-			var lineSubPathArray = new SubPathInfo();
-			var lineArray = new Array(new PathPointInfo, new PathPointInfo, new PathPointInfo, new PathPointInfo);
+		createCycle : function(r, cPosition) {
+			var subPath = new SubPathInfo();
+			var points = new Array(new PathPointInfo, new PathPointInfo, new PathPointInfo, new PathPointInfo);
+			var roundPathLayer;
 			var directionDistance = 0;
 
 			if(!(cPosition instanceof Array)) {
-				cPosition = [size / 2, size / 2];
+				cPosition = [r, r];
 			}
-			if(typeof size !== 'number') {
-				size = 10;
+			if(typeof r !== 'number') {
+				r = 10;
 			}
 			if(typeof cPosition[0] !== 'number') {
-				cPosition[1] = size / 2;
+				cPosition[1] = r;
 			}
 			if(typeof cPosition[1] !== 'number') {
-				cPosition[1] = size / 2;
+				cPosition[1] = r;
 			}
-			directionDistance = size / 4;
+			directionDistance = r / Math.tan(Math.PI / (180 / 61));
 
-			lineArray[0].kind = PointKind.SMOOTHPOINT;
-			lineArray[0].anchor = [cPosition[0], cPosition[1] - size / 2];
-			lineArray[0].leftDirection = [cPosition[0] + directionDistance, cPosition[1] - size / 2];
-			lineArray[0].rightDirection = [cPosition[0] - directionDistance, cPosition[1] - size / 2];
+			points[0].kind = PointKind.SMOOTHPOINT;
+			points[0].anchor = [cPosition[0], cPosition[1] - r];
+			points[0].leftDirection = [cPosition[0] + directionDistance, cPosition[1] - r];
+			points[0].rightDirection = [cPosition[0] - directionDistance, cPosition[1] - r];
 
-			lineArray[1].kind = PointKind.SMOOTHPOINT;
-			lineArray[1].anchor = [cPosition[0] + size / 2, cPosition[1]];
-			lineArray[1].leftDirection = [cPosition[0] + size / 2, cPosition[1] + directionDistance];
-			lineArray[1].rightDirection = [cPosition[0] + size / 2, cPosition[1] - directionDistance];
+			points[1].kind = PointKind.SMOOTHPOINT;
+			points[1].anchor = [cPosition[0] + r, cPosition[1]];
+			points[1].leftDirection = [cPosition[0] + r, cPosition[1] + directionDistance];
+			points[1].rightDirection = [cPosition[0] + r, cPosition[1] - directionDistance];
 
-			lineArray[2].kind = PointKind.SMOOTHPOINT;
-			lineArray[2].anchor = [cPosition[0], cPosition[1] + size / 2];
-			lineArray[2].leftDirection = [cPosition[0] - directionDistance, cPosition[1] + size / 2];
-			lineArray[2].rightDirection = [cPosition[0] + directionDistance, cPosition[1] + size / 2];
+			points[2].kind = PointKind.SMOOTHPOINT;
+			points[2].anchor = [cPosition[0], cPosition[1] + r];
+			points[2].leftDirection = [cPosition[0] - directionDistance, cPosition[1] + r];
+			points[2].rightDirection = [cPosition[0] + directionDistance, cPosition[1] + r];
 
-			lineArray[3].kind = PointKind.SMOOTHPOINT;
-			lineArray[3].anchor = [cPosition[0] - size / 2, cPosition[1]];
-			lineArray[3].leftDirection = [cPosition[0] - size / 2, cPosition[1] - directionDistance];
-			lineArray[3].rightDirection = [cPosition[0] - size / 2, cPosition[1] + directionDistance];
+			points[3].kind = PointKind.SMOOTHPOINT;
+			points[3].anchor = [cPosition[0] - r, cPosition[1]];
+			points[3].leftDirection = [cPosition[0] - r, cPosition[1] - directionDistance];
+			points[3].rightDirection = [cPosition[0] - r, cPosition[1] + directionDistance];
 
-			lineSubPathArray.closed = true;
-			lineSubPathArray.operation = ShapeOperation.SHAPEADD;
-			lineSubPathArray.entireSubPath = lineArray;
-			var myPathItem = app.activeDocument.pathItems.add("myPath", [lineSubPathArray]);
+			subPath.closed = true;
+			subPath.operation = ShapeOperation.SHAPEADD;
+			subPath.entireSubPath = points;
+			roundPathLayer = app.activeDocument.pathItems.add('roundPath', [subPath]);
 
 			var desc88 = new ActionDescriptor();
 			var desc89 = new ActionDescriptor();
 			var desc90 = new ActionDescriptor();
 			var desc91 = new ActionDescriptor();
 			var ref60 = new ActionReference();
-			var id481 = charIDToTypeID("RGBC");
 
-			ref60.putClass(stringIDToTypeID("contentLayer"));
-			desc88.putReference(charIDToTypeID("null"), ref60);
+			ref60.putClass(stringIDToTypeID('contentLayer'));
+			desc88.putReference(charIDToTypeID('null'), ref60);
 
-			desc91.putDouble(charIDToTypeID("Rd  "), 0.000000); // R
-			desc91.putDouble(charIDToTypeID("Grn "), 0.000000); // G
-			desc91.putDouble(charIDToTypeID("Bl  "), 0.000000); // B
-			desc90.putObject(charIDToTypeID("Clr "), id481, desc91);
-			desc89.putObject(charIDToTypeID("Type"), stringIDToTypeID("solidColorLayer"), desc90);
-			desc88.putObject(charIDToTypeID("Usng"), stringIDToTypeID("contentLayer"), desc89);
+			desc91.putDouble(charIDToTypeID('Rd  '), 0.000000);
+			desc91.putDouble(charIDToTypeID('Grn '), 0.000000);
+			desc91.putDouble(charIDToTypeID('Bl  '), 0.000000);
+			desc90.putObject(charIDToTypeID('Clr '), charIDToTypeID('RGBC'), desc91);
+			desc89.putObject(charIDToTypeID('Type'), stringIDToTypeID('solidColorLayer'), desc90);
+			desc88.putObject(charIDToTypeID('Usng'), stringIDToTypeID('contentLayer'), desc89);
 
-			executeAction(charIDToTypeID("Mk  "), desc88, DialogModes.NO);
-			myPathItem.remove();
+			executeAction(charIDToTypeID('Mk  '), desc88, DialogModes.NO);
+			roundPathLayer.remove();
 		}
 	},
 	selection : {
@@ -123,7 +123,7 @@
 			spi.operation = ShapeOperation.SHAPEXOR;
 			spi.entireSubPath = [startPoint, stopPoint];
 
-			line = app.activeDocument.pathItems.add("Line", [spi]);
+			line = app.activeDocument.pathItems.add('Line', [spi]);
 			line.strokePath(ToolType.PENCIL);
 			line.remove();
 		},
@@ -150,4 +150,4 @@
 
 //drawLine([100,100], [200,200]);
 
-Shape.vector.createCycle(200);
+Shape.vector.createCycle(50);
